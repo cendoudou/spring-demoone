174c3c1ca5b57b5227d9fd523a9fff220ec3d09d
doudou <doudou@imooc.com>
2020-10-05 21:53:49 +0800

    开发人员提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//我是开发人员，还没提交
 @SpringBootApplication
 public class DemoApplication {
 
